ab5cacfdf589a6b7c26f04ca3a787a80fe753a48
Cotofey11 <nazarpikas12@gmail.com>
2025-08-26 16:16:57 +0300

    Refactor loop with enumerate, add docstrings, fix minor issues

---
+++ Lesson_7/Task_2/Lesson_7_Task_2.py
@@ -0,0 +1,11 @@
+"""Задание: Напишите программу, которая имитирует обратный отсчет перед запуском
+ракеты. Программа должна запросить у пользователя число (например, 5),
+а затем вывести отсчет от этого числа до 1, после чего напечатать "Go!"."""
+user_number : int = int(input("Enter the number of seconds for the countdown: "))
+while user_number <= 0:
+        user_number = int(input("Incorrect value! Please, Try Again!"
+                                " Enter the number of seconds for the countdown: "))
+while user_number > 0:
+    print(f"{user_number}...")
+    user_number -= 1
+print("Go!!!!!!!")
